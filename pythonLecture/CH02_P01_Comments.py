e2f97ba0d74e3e32212d64d324feb84194f40df3
GkHell <gpkmyworld@gmail.com>
2021-12-02 16:37:24 +0530

    02/12/2021⠀⎦⎡⠀16:37⠀⎦⎢⠀Thursday

---
+++ pythonLecture/CH02_P01_Comments.py
@@ -0,0 +1,25 @@
+
+#djfvdj ijd dfkj idf kj ;
+
+a = '''
+idbvsd dfnk 
+
+df;vndf
+dkfjndf
+
+ddjbfk kjdf k 
+
+'''
+
+# 3434344324324
+# 234324
+# 324234
+# 23432
+4
+234
+23
+4
+234
+23
+
+print("Hello World");
