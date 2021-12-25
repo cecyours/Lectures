6fadc4a020b5fa3e7732485b712e063f83ff9831
GkHell <gpkmyworld@gmail.com>
2021-12-25 10:32:38 +0530

    25/12/2021⠀⎦⎡⠀10:32⠀⎦⎢⠀Saturday

---
+++ JavaProgramming/CH01_P02_SimpleInterest.java
@@ -0,0 +1,28 @@
+// SI = PRN/100
+/**
+ * System.out => display(monitor)
+ *  System.in => keyboard  
+ * 
+ * */
+import java.util.*;
+class CH01_P02_SimpleInterest{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		float SI;
+		float R = 10.35f; // f represent float, bcz decimals are double by - default.
+		float P,N;
+
+		System.out.print("Enter amount : ");
+		P = sc.nextFloat();
+
+		System.out.print("Enter duration : ");
+		N = sc.nextFloat();
+
+		SI = P*R*N/100;
+
+
+		System.out.printf("SI : %f\n",SI);
+			
+	}
+}
