fc9065a074bf6dff6c715253e0cf398c0fbecede
gkHell <gpkmyworld@gmail.com>
2022-03-15 05:20:34 -0700

    15/03/2022⠀⎦⎡⠀05:20⠀⎦⎢⠀Tuesday

--- JavaCodersBatch5_8/CH01_P01_Dog.java
+++ JavaCodersBatch5_8/CH01_P01_Dog.java
@@ -2,10 +2,23 @@
 class CH01_P01_Dog{
 	public static void main(String[] args) {
 		
+		Dog d = new Dog();
+		d.name="Tommy";
+		d.barking();
 	}
 }
 
 
 class Dog{
 
+	/** properties*/
+	String name;
+	int height;
+
+
+	/**behaviour*/
+	void barking()
+	{
+		System.out.println(name+" is barking....");
+	}
 }
