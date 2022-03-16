6435254a4cd47033ffed16e1397264c3aa9fed39
gkHell <gpkmyworld@gmail.com>
2022-03-16 05:59:40 -0700

    16/03/2022⠀⎦⎡⠀05:59⠀⎦⎢⠀Wednesday

---
+++ JavaCodersBatch5_8/prince111.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class prince111{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    	 int a,b;
+    System.out.print("Enter Number : ");
+     a = sc.nextInt();
+     b = a;
+
+
+
+    }
+
+}
---
+++ JavaCodersBatch5_8/student09.java
@@ -0,0 +1,21 @@
+class student09{
+   public static void main(String[] args) {
+   	
+   	  student s = new student();
+   	  s.name = "prince";
+   	  s.studying();
+   	}	
+
+}
+
+class student{
+    String name;
+    String height;
+
+
+    void studying()
+    {
+    	System.out.println(name + "  is studying.....");
+    }
+
+}
