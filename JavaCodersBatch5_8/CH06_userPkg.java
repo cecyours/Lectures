587cf7b0a24821209959793f40423fa202d1c55e
gkHell <gpkmyworld@gmail.com>
2022-03-24 18:36:23 -0700

    24/03/2022⠀⎦⎡⠀18:36⠀⎦⎢⠀Thursday

---
+++ JavaCodersBatch5_8/CH06_userPkg.java
@@ -0,0 +1,12 @@
+
+import say_me.Print;
+class CH06_userPkg{
+	public static void main(String[] args) {
+		
+		Print.sop("Hello There..ds..");
+		Print.sop("Hello There...sd.");
+		Print.sop("Hello There.dv...");
+		Print.sop("Hello There.cv.sd..");
+
+	}
+}
