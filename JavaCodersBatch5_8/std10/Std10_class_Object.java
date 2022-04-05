6cbadac0c44f66cac5c5b8889ee3f0c88f1212a1
gkHell <gpkmyworld@gmail.com>
2022-04-05 06:32:05 -0700

    05/04/2022⠀⎦⎡⠀06:31⠀⎦⎢⠀Tuesday

--- JavaCodersBatch5_8/std10/Std10_class_Object.java
+++ JavaCodersBatch5_8/std10/Std10_class_Object.java
@@ -3,7 +3,10 @@ class Std10_class_Object{
 	public static void main(String[] args) {
 		
 		Student s1 = new Student();
-		s1.rollno = 10;
+		
+		// S.o.p("Enter roll No : ")
+		s1.rollno = // sc.nextInt();
+
 		s1.name = "rohit";
 		s1.marks = 89.33;
 		s1.display();
