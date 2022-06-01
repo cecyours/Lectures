06ee071d6cec90c7c8956116b228323daf151522
gkHell <gpkmyworld@gmail.com>
2022-06-01 12:01:10 -0700

    01/06/2022⠀⎦⎡⠀12:00⠀⎦⎢⠀Wednesday

---
+++ C_Language_Batch0342/pratice08.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+	int i,j,n;
+	
+	printf("Enter the number : ");
+	scanf("%d",&n);
+	
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			printf("   ");
+		}
+			printf(" * ");
+		if(i%3==0)
+			printf("   ");
+
+	}
+}
