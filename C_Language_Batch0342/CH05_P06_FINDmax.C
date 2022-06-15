c53a5bbf606e2ca6be7f51df524c5e67e90742c0
gkHell <gpkmyworld@gmail.com>
2022-06-14 22:42:13 -0700

    14/06/2022⠀⎦⎡⠀22:42⠀⎦⎢⠀Tuesday

---
+++ C_Language_Batch0342/CH05_P06_FINDmax.C
@@ -0,0 +1,33 @@
+
+#include<stdio.h>
+int main()
+{
+	int a[100],i,n;
+	int max=0,min=0;
+	
+	
+	printf("enter size : ");
+	scanf("%d",&n);
+	
+	printf("Enter elements : \n");
+	for(i=0;i<n;i++)
+	{
+		printf("a[ %d ] = ",i);
+		scanf("%d",&a[i]);
+	}
+	
+	printf("------------\n");
+	min=max=a[0];
+	for(i=0;i<n;i++)
+	{
+		if(max<a[i])
+		{
+			max = a[i];
+		}
+		if(min>a[i])
+		{
+			min = a[i];
+		}
+	}
+	printf("min : %d , max : %d\n",min,max);
+}
