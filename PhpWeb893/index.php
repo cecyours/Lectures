0523472f9007d3f847867b3ac377592bb62e6dd2
gkHell <gpkmyworld@gmail.com>
2022-07-18 23:23:33 +0530

    18/07/2022⠀⎦⎡⠀23:23⠀⎦⎢⠀Monday

---
+++ PhpWeb893/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="./Styles/main.css">
+
+	<style type="text/css">
+		h1{
+			background: white;
+		}
+	</style>
+</head>
+<body>
+	
+
+	<a href="AllTopis.php">ALl IMP TOPICS</a>
+</body>
+</html>
