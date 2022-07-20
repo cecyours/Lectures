fcc67217eafdb7f6f6478b864f5aeaed5ffd11c9
gkHell <gpkmyworld@gmail.com>
2022-07-20 23:34:46 +0530

    20/07/2022⠀⎦⎡⠀23:34⠀⎦⎢⠀Wednesday

--- JavaProgrammer7831/Lec10_P02_strMethod_char_CompareTo.java
+++ JavaProgrammer7831/Lec10_P02_strMethod_char_CompareTo.java
@@ -3,6 +3,9 @@ class Lec10_P02_strMethod_char_CompareTo {
 	public static void main(String[] args) {
 		
 		String str = "hello";
-		
+
+		char ch = str.charAt(1);
+
+		System.out.println("Character : "+ch);
 	}
 }
