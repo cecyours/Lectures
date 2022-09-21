2e9d9d0f0c909f691dbbcc8dfd92ef3605e70354
Gk Sir <gpkmyworld@gmail.com>
2022-09-21 16:29:39 +0530

    21/09/2022⠀⎦⎡⠀16:29⠀⎦⎢⠀Wednesday

---
+++ C_Language_Batch0342/NodeRevision_LinkedList.c
@@ -0,0 +1,65 @@
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+struct Node{
+
+	char name[100];
+	struct Node *next = NULL;
+}*head = NULL;
+
+
+void insert(char name[])
+{
+	struct Node *newNode,*temp;
+
+	newNode = (struct Node *)malloc(sizeof(struct Node *));
+
+	strcpy(newNode->name,name);
+
+	if(head==NULL)
+	{
+		head = newNode;
+		return;
+	}
+	temp = head;
+
+	while(temp->next!=NULL)
+	{
+		temp = temp->next;
+	}
+
+	temp->next = newNode;
+
+	printf("node creaded ...\n");
+}
+
+void display()
+{
+	struct Node *temp;
+
+	if(head==NULL)
+	{
+		printf("no data ,,\n");
+		return;
+	}
+
+	temp = head;
+
+	while(temp)
+	{
+		printf("%s\n", temp->name);
+		temp = temp->next;
+	}
+}
+int main()
+{
+	char n[] = "Gk";
+	insert(n);
+	insert(n);
+	insert(n);
+	insert(n);
+	insert(n);
+
+	display();
+}
---
+++ C_language_9938/CH04_P03_WhileLoop.c
@@ -0,0 +1,13 @@
+
+#include<stdio.h>
+int main()
+{
+	int num;
+
+	num = 1;
+	while(num<=100)//condition..
+	{
+		printf(" %d ", num);
+		num++;
+	}
+}
