29ff896224635f2e034c67535de669260404c210
Gk Sir <gpkmyworld@gmail.com>
2022-09-22 10:03:56 +0530

    22/09/2022⠀⎦⎡⠀10:03⠀⎦⎢⠀Thursday

--- C_Language_Batch0342/pratice23.c
+++ C_Language_Batch0342/pratice23.c
@@ -14,7 +14,7 @@ void insert(char name[])
 	struct Node *newNode,*temp;
 
 	newNode = (struct Node *)malloc(sizeof(struct Node *));
-	
+
 	strcpy(newNode->name,name);
 
 	if(head==NULL)
@@ -29,11 +29,68 @@ void insert(char name[])
 		temp = temp->next;
 	}
 
-	temp->next = newNode
+	temp->next = newNode;
 
 	printf("node creaded ...\n");
 }
 
+void search(char name[])
+{
+	int f = 0;
+	struct Node *temp;
+
+	if(head==NULL)
+	{
+		printf("no data to search...\n");
+		return;
+	}
+
+	temp = head;
+
+	while(temp)
+	{
+		if(strcmp(temp->name,name)==0)
+		{
+			f =1;
+			printf("data mil gya ..\n");
+			break;
+		}
+		temp = temp->next;
+	}
+
+	if(!f)	
+	{
+		printf("no data found...\n");
+	}
+}
+
+void delete_(char name[])
+{
+int f = 0;
+	struct Node *temp;
+
+	if(head==NULL)
+	{
+		printf("no data to search...\n");
+		return;
+	}
+
+	temp = head;
+
+	while(temp)
+	{
+		if(strcmp(temp->next->name,name)==0)
+		{
+			 
+			 temp->next = temp->next->next;
+			 printf("data deleted....\n");
+			break;
+		}
+		temp = temp->next;
+	}
+
+
+}
 void display()
 {
 	struct Node *temp;
@@ -54,12 +111,18 @@ void display()
 }
 int main()
 {
-	char n[] = "Gk";
-	insert(n);
-	insert(n);
-	insert(n);
-	insert(n);
-	insert(n);
+	char n1[] = "Avi 1";
+	char n2[] = "Avi 2";
+	char n3[] = "Avi 3";
+	char n4[] = "Avi 4";
+	char n5[] = "Avi 5";
+	insert(n1);
+	insert(n2);
+	insert(n3);
+	insert(n4);
+	insert(n5);
 
+	search(n1);
+	delete_(n3);
 	display();
 }
