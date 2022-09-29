78cf056044b0c65067da1ed199c0e9385f72e325
Gk Sir <gpkmyworld@gmail.com>
2022-09-29 10:33:23 +0530

    29/09/2022⠀⎦⎡⠀10:33⠀⎦⎢⠀Thursday

--- C_language_9938/CH04_P11_gotoStatement.c
+++ C_language_9938/CH04_P11_gotoStatement.c
@@ -5,15 +5,15 @@ int main()
 	printf("level 1\n");
 	printf("level 2\n");
 	printf("level 3\n");
-	printf("level 4\n");
+	printf("level 4\n/");
+you:
 
-goto you;
 	printf("level 5\n");
 	printf("level 6\n");
 	printf("level 7\n");
 	printf("level 8\n");
 
-you:
+goto you;
 	printf("level 9\n");
 	printf("level 10\n");
 }
