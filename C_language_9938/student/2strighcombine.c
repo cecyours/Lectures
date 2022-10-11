caddd3a249ca45672de2223ab507434511e83a16
Gk Sir <gpkmyworld@gmail.com>
2022-10-12 03:25:33 +0530

    12/10/2022⠀⎦⎡⠀03:25⠀⎦⎢⠀Wednesday

---
+++ C_language_9938/student/2strighcombine.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+	char str1[] = "hello";
+	char str2[] = "bye";
+
+	strcat(str1,"GK");
+
+	printf("Wellcome :)\n\n%s",str1 );
+}
---
+++ html/2stringh.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+	char str1[] = "hello";
+	char str2[] = "bye";
+
+	strcat(str1,str2);
+
+	printf("Wellcome :)%s",str1 );
+}
