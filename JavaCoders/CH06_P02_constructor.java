cb2a365c25fb66d32442a0102a6b2614a7f6f25c
Gk Sir <gpkmyworld@gmail.com>
2022-11-04 10:48:58 +0530

    04/11/2022⠀⎦⎡⠀10:48⠀⎦⎢⠀Friday

---
+++ JavaCoders/CH06_P02_constructor.java
@@ -0,0 +1,37 @@
+
+class CH06_P02_constructor  {
+	public static void main(String[] args) {
+		 
+		new Task("SUMIT"); //t3
+		new Task(20);//t2
+		new Task();
+		new Task(8,9,8+9); //?
+		//t.display();
+	}
+}
+
+class Task{
+
+	public Task() // t1
+	{
+		System.out.println("Happy Me..");
+	}
+
+	public Task(int a) //t2
+	{
+
+		System.out.println("Hiii");
+	}
+
+
+	public Task(String code) //t3
+	{
+		System.out.println("Bye bye..");
+	}
+
+	public Task(int a,int b,int c)
+	{
+		System.out.println(a+" + "+b+" = "+c);
+	}
+	 
+}
