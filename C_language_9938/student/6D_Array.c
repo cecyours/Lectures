f61ccf6f332f207fb18749842b2aa875ffdaa4f4
Gkhell <gpkmyworld@gmail.com>
2022-11-25 19:18:32 +0530

    25/11/2022⠀⎦⎡⠀19:18⠀⎦⎢⠀Friday

--- C_language_9938/student/6D_Array.c
+++ C_language_9938/student/6D_Array.c
@@ -1,56 +1,56 @@
+
+
+
+
+								a[z][x][c][v][b][n] = ++counter;
+								printf("%6d",a[z][x][c][v][b][n]);
+							for(n=0;n<y;n++)
+							for(n=0;n<y;n++)
+							printf("\t");
+							{
+							{
+							}
+							}
+						for(b=0;b<t;b++)
+						for(b=0;b<t;b++)
+						printf(" - ");
+						{
+						{
+						}
+						}
+					for(v=0;v<r;v++)
+					for(v=0;v<r;v++)
+					printf("\n");
+					{
+					{
+					}
+					}
+				for(c=0;c<e;c++)
+				for(c=0;c<e;c++)
+				{
+				{
+				}
+				}
+			{
+			{
+			}
+			}
+		for(x=0;x<w;x++)
+		for(x=0;x<w;x++)
+	for(z=0;z<q;z++)
+	for(z=0;z<q;z++)
+	int a[10][10][10][10][10][10];
+	int counter = 0;
+	int q,w,e,r,t,y;
+	int z,x,c,v,b,n;
+	printf("---------------------------\n");
+	printf("Enter q-w-e-r-t-y ;");
+	scanf("%d-%d-%d-%d-%d-%d",&q,&w,&e,&r,&t,&y);
+	{
+	{
+	}
+	}
 #include<stdio.h>
 int main()
 {
-	int a[10][10][10][10][10][10];
-	int z,x,c,v,b,n;
-	int q,w,e,r,t,y;
-	int counter = 0;
-
-	printf("Enter q-w-e-r-t-y ;");
-	scanf("%d-%d-%d-%d-%d-%d",&q,&w,&e,&r,&t,&y);
-
-	for(z=0;z<q;z++)
-	{
-		for(x=0;x<w;x++)
-			{
-				for(c=0;c<e;c++)
-				{
-					for(v=0;v<r;v++)
-					{
-						for(b=0;b<t;b++)
-						{
-							for(n=0;n<y;n++)
-							{
-								a[z][x][c][v][b][n] = ++counter;
-							}
-						}
-					}
-				}
-			}
-	}
-	printf("---------------------------\n");
-
-
-	for(z=0;z<q;z++)
-	{
-		for(x=0;x<w;x++)
-			{
-				for(c=0;c<e;c++)
-				{
-					for(v=0;v<r;v++)
-					{
-						for(b=0;b<t;b++)
-						{
-							for(n=0;n<y;n++)
-							{
-								printf("%6d",a[z][x][c][v][b][n]);
-							}
-							printf("\t");
-						}
-						printf(" - ");
-					}
-					printf("\n");
-				}
-			}
-	}
 }
