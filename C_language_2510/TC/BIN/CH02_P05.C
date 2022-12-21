83c80ed933c02747d8910d0ebb4170b6e3558dc6
RAVAN0248 <sagarjigar87gmail.com>
2022-12-21 11:51:53 +0530

    21/12/2022⠀⎦⎡⠀11:51⠀⎦⎢⠀Wednesday

---
+++ C_language_2510/TC/BIN/CH02_P05.C
@@ -0,0 +1,15 @@
+void main()
+{
+	char name[100];
+clrscr();
+	printf("Enter your name : ");
+       //	scanf("%s",name);
+	gets(name);
+
+	printf("Welcome\n");
+	puts(name);
+
+	putchar('8');
+
+getche();
+}
---
+++ C_language_2510/TC/BIN/CH2_PRAC.BAK
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int i = 99;
+clrscr();
+		printf("default value : %d\n",i);
+
+		i = 101;
+		printf("new value : %d\n",i);
+
+getch();
+
+}
