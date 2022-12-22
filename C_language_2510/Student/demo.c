2cbb351f818f7deb707bc7a56afe512b0e7fde41
gkHell <gpkmyworld@gmail.com>
2022-12-22 11:51:46 +0530

    new file:   C_language_2510/CH03_P02_relatational.c

---
+++ C_language_2510/Student/demo.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+
+    int a,b;
+
+            printf("enter 2 number = ");
+            scanf("%d,%d",&a,&b);
+
+            printf("%d > %d : %d\n",a,b,a>b);
+            printf("%d < %d : %d\n",a,b,a<b);
+            printf("%d >=%d : %d\n",a,b,a>=b);
+            printf("%d <=%d : %d\n",a,b,a<=b);
+            printf("%d !=%d : %d\n",a,b,a!=b);
+            printf("%d ==%d : %d\n",a,b,a==b);
+
+            return(0);
+
+
+
+
+}
