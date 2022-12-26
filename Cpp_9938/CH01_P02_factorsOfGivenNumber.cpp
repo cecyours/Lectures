d2459248a275715eaa87c1dd2778e86e5257ddf2
gkHell <gpkmyworld@gmail.com>
2022-12-26 09:06:24 +0530

    26/12/2022⠀⎦⎡⠀09:06⠀⎦⎢⠀Monday

---
+++ Cpp_9938/CH01_P02_factorsOfGivenNumber.cpp
@@ -0,0 +1,47 @@
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num;
+    bool is_prime=true;
+    cout<<"Enter the number : ";
+    cin>>num;
+
+    int temp = num;
+
+        int factor = 1;
+    while(temp>1)
+    {
+        is_prime = true;
+        
+        for(int i=2;i<=factor/2;i++)
+        {
+            if(factor%i==0)
+            {
+                 is_prime = false;
+                break;
+            }
+        }
+
+        if(is_prime)
+        {
+            if(temp%factor==0)
+            {
+                temp = temp/factor;
+                if(temp!=1)
+                 cout<<factor<<"x";
+                else
+                 cout<<factor<<".";
+            }
+        
+        
+        
+        
+        }
+                factor++;
+        
+  
+    
+    }
+}
