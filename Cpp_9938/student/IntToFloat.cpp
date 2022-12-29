9b5e0e12d29eecbf86b496024a7465e09a58bf8c
gkHell <gpkmyworld@gmail.com>
2022-12-29 08:58:22 +0530

    29/12/2022⠀⎦⎡⠀08:58⠀⎦⎢⠀Thursday

---
+++ Cpp_9938/student/IntToFloat.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i;
+    float f;
+
+    cout<<"Enter a number :";
+    cin>> f;
+
+
+    i = f;
+
+    cout<<f<<" - "<<i<<" = "<<f-i<<endl;
+
+    return 0;
+
+
+}
---
+++ Cpp_9938/student/implicit_explicit.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i = 125;
+    float f;
+
+    f = i;
+
+    cout<<" int "<<i<<endl;
+    cout<<"float"<<f<<endl;
+
+    f = 150.99;
+
+    i = f;
+
+    cout<<" int "<<i<<endl;
+    cout<<"float"<<f<<endl;
+
+
+    return 0;
+
+}
