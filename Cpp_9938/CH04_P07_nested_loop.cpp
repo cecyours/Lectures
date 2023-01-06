b0b658b81e1d6fd423c96978666b585e3455f00c
gkHell <gpkmyworld@gmail.com>
2023-01-06 18:56:22 +0600

    06/01/2023⠀⎦⎡⠀18:56⠀⎦⎢⠀Friday

---
+++ Cpp_9938/CH04_P07_nested_loop.cpp
@@ -0,0 +1,22 @@
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int start,end,i,j;
+
+
+        cout<<"Enter start end : ";
+        cin>>start>>end;
+
+        for(i=start;i<=end;i++)
+        {
+            cout<<" ------ "<<i<<" -- -----"<<endl;
+            int n = i;
+            for(j=1;j<=10;j++)
+            {
+                cout<<n<<" x "<<j<<" = "<<(n*j)<<endl;
+            }
+        }
+    return 0;
+}
