5a21732f4df2ec0e2c8ee883afeede15941e6139
nova-hash <surajgupta929479@gmail.com>
2023-01-09 19:55:26 +0530

    09/01/2023⠀⎦⎡⠀19:55⠀⎦⎢⠀Monday

---
+++ Cpp_9938/student/table.cpp
@@ -0,0 +1,34 @@
+
+#include<iostream>
+using namespace std;
+int main()
+{   
+    int start,end,i,j;
+
+    cout<<"Enter start end : ";
+    cin >>start>>end;
+
+    
+    for(j=1;j<=10;j++)
+        {
+
+           
+            int n = i;
+        
+                 for(i=start;i<=end;i++)
+                    {
+
+                    
+                         cout<<i<<" X "<<j<<" = "<<(i*j)<<"\t";
+                         
+                        
+                        
+                    }
+                    cout<<endl;
+        
+        }
+   
+
+    return 0;
+
+}
