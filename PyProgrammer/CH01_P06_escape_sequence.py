170eac5d1c6c72ca2e579e57709f1db1f714cc05
gkHell <gpkmyworld@gmail.com>
2023-01-11 05:53:13 +0600

    11/01/2023⠀⎦⎡⠀05:53⠀⎦⎢⠀Wednesday

--- PyProgrammer/CH01_P06_escape_sequence.py
+++ PyProgrammer/CH01_P06_escape_sequence.py
@@ -3,6 +3,7 @@
 ## \n : new line
 ## \b : backspace
 ## \f : form feed
+## \a : alert sound
 ## \\ : display special escape sequence
 print("1234\b5\t678\n90");
 
@@ -11,3 +12,14 @@ print("12345\f678\f90")
 print("\\n");
 
 print("Hello \"Rupesh\" ")
+
+print("123\a45\f678\f90")
+import time
+time.sleep(1)
+print("123\a45\f678\f90")
+time.sleep(1)
+
+print("123\a45\f678\f90")
+time.sleep(1)
+
+print("123\a45\f678\f90")
