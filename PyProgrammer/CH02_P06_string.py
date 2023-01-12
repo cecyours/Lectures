f1aa98ddbd0c7cde5ac07ceae5f90b3a9183b4e0
gkHell <gpkmyworld@gmail.com>
2023-01-12 19:40:56 +0600

    12/01/2023⠀⎦⎡⠀19:40⠀⎦⎢⠀Thursday

---
+++ PyProgrammer/CH02_P06_string.py
@@ -0,0 +1,26 @@
+
+name1 = 'Rupesh'
+name2 = "coder"
+
+
+print(name1,name2)
+
+msg = """Python is a high-level,
+                general-purpose programming language.
+  Its design philosophy emphasizes code readability with the use of significant indentation.
+   Python is dynamically-typed and garbage-collected. 
+   It supports multiple programming paradigms, 
+   including structured, object-oriented and functional"""
+print(msg)
+
+print("---------------------------------")
+
+s = "Kuch Bhi";
+print(s[2:])
+print(s[:2])
+print(s[::-1])
+
+# start-end-step
+
+s = "Computer Education & Cybernetics"
+print(s[10:1:-1])
