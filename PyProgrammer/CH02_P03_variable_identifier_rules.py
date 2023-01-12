c4f13f66f9d20e3f7be7d78a855ca4c7f6dae380
gkHell <gpkmyworld@gmail.com>
2023-01-12 19:11:09 +0600

    12/01/2023⠀⎦⎡⠀19:11⠀⎦⎢⠀Thursday

---
+++ PyProgrammer/CH02_P03_variable_identifier_rules.py
@@ -0,0 +1,8 @@
+fosdr_ = "r"
+# literal 'r':
+# variable  name 
+# ------- RULES
+# digits allowed bt not at first position
+# doesn't contains special symbols except under score '_'
+# keywords not allowed.
+# alphabets + digits + under score
