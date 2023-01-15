1acd7363086ae2c0cf395819fe3127ff4020eef6
gkHell <gpkmyworld@gmail.com>
2023-01-16 05:55:45 +0600

    16/01/2023⠀⎦⎡⠀05:55⠀⎦⎢⠀Monday

---
+++ PyProgrammer/CH02_10_set_operation.py
@@ -0,0 +1,19 @@
+
+data = {1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0};
+
+print(data)
+
+
+data = []
+
+for i in range(5):
+    name = input("Enter your name : ");
+    data.append(name)
+
+print(data)
+
+s = set(data)
+
+print(s)
+
+print(set({"apple","app","aps"}))
