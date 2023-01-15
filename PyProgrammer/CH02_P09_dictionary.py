6345129dde8ea5ae8328179e21305894145abb2d
gkHell <gpkmyworld@gmail.com>
2023-01-16 05:46:38 +0600

    16/01/2023⠀⎦⎡⠀05:46⠀⎦⎢⠀Monday

--- PyProgrammer/CH02_P09_dictionary.py
+++ PyProgrammer/CH02_P09_dictionary.py
@@ -1,13 +1,24 @@
-
+import os
 
 data = {"Mukesh":99.00,"Heena":53.64,"Rupesh":99.75,"Keyboard":77.88};
 
-print(data)
 
 
-name = input("Enter your name : ");
+want = True
 
-try:
-    print(name," your marks is", data[name]);
-except :
-    print("invalid name...")
+while want:
+    os.system("clear");
+    print(data)
+    name = input("Enter your name : ");
+
+    try:
+        print(name," your marks is", data[name]);
+    except :
+        print("invalid name...")
+    
+    choice = input("do you want to repeat [Y/N]: ")
+    
+    if choice == "Y" or choice =='y':
+        want = True
+    else:
+        want = False
