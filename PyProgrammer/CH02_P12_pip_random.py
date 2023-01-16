95e6f6f9e3dfda450cb3fb180fd79fc5bae6f78f
gkHell <gpkmyworld@gmail.com>
2023-01-17 05:08:48 +0600

    17/01/2023⠀⎦⎡⠀05:08⠀⎦⎢⠀Tuesday

---
+++ PyProgrammer/CH02_P12_pip_random.py
@@ -0,0 +1,16 @@
+
+import random
+
+alpha = ["Heena","Mohan","Pappu","Rohan"]
+
+beta = ["Mukesh","Ramesh","Chagan"]
+
+
+# data = random.randrange(1,9)
+# print(data)
+
+
+player1 = random.choice(alpha)
+player2 = random.choice(beta)
+
+print(player1,"\t",player2)
