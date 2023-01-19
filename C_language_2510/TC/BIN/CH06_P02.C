1122f80480290f6125ef69775d5c22c0b2700819
gkHell <gpkmyworld@gmail.com>
2023-01-19 23:19:03 +0600

    19/01/2023⠀⎦⎡⠀23:19⠀⎦⎢⠀Thursday

--- C_language_2510/TC/BIN/CH06_P02.C
+++ C_language_2510/TC/BIN/CH06_P02.C
@@ -5,6 +5,7 @@
 void main()
 {
 	char str1[100]="Hello HaPPPY",str2[100]="World";
+	char *ptr;
 	int len,status;
  clrscr();
 	len = strlen(str1);
@@ -29,6 +30,33 @@ void main()
        status = strcmp(str1,str2);
        printf("compare status : %d\n",status);
 
+	       strcpy(str1,"Shivam");
+		ptr = strchr(str1,'v');
 
+	      if(ptr==NULL)
+	      {
+		printf("character can't be fine.\n");
+	      }
+	      else
+	      {
+		printf("Character find success fully.\n");
+	      }
+
+	      ptr = strstr(str1,"ivg");
+
+	      if(ptr==NULL)
+	      {
+		printf("string can't be find.\n");
+	      }
+	      else
+	      {
+		printf("string find success fully.\n");
+	      }
+
+	      strrev(str1);
+	      printf("reverse string : %s\n",str1);
+
+	      strset(str1,3);
+	      printf("String set : %s\n",str1);
  getch();
 }
