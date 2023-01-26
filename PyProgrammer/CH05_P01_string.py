324bd10f9f91d0e19ed2c3186a345e63ada4c4e3
gkHell <gpkmyworld@gmail.com>
2023-01-27 05:14:24 +0530

    27/01/2023⠀⎦⎡⠀05:14⠀⎦⎢⠀Friday

---
+++ PyProgrammer/CH05_P01_string.py
@@ -0,0 +1,37 @@
+
+s = "Hello World"
+mul ="""Hi there
+
+    i am gk!....
+"""
+
+
+print(s,mul)
+
+s = "Rupesh World"
+
+
+# string accessing.
+print(s,s[2],sep=" | ")
+
+# slicing..
+
+# forward slice
+print(s,s[1:5],sep=" | ")
+
+# step slice
+print(s,s[1:9:2],sep=" | ")
+
+# string
+print(s,s[::],sep=" | ")
+
+# to end till 5 
+print(s,s[:5],sep=" | ")
+
+# to start from 2
+print(s,s[2:],sep=" | ")
+
+# from last string
+print(s,s[-5],sep=" | ")
+
+# reverse 
