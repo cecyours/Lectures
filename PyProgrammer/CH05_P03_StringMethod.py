01c367cc50cc7c34e689f58946840d07a230d632
gkHell <gpkmyworld@gmail.com>
2023-01-28 05:03:47 +0530

    28/01/2023⠀⎦⎡⠀05:03⠀⎦⎢⠀Saturday

---
+++ PyProgrammer/CH05_P03_StringMethod.py
@@ -0,0 +1,19 @@
+
+
+s = input("Enter your String : ")
+
+print(s,s.capitalize(),sep=" | ")
+
+
+print(s,s.casefold(),sep=" | ")
+
+print(s,s.center(10,'*'),sep=" | ")
+
+print(s,s.count('a'),sep=" | ")
+
+print(s,s.encode(),sep=" | ")
+
+x = "Ha\tppy\tme"
+print(x,x.expandtabs(2),sep=" | ")
+
+
