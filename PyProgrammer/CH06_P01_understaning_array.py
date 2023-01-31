bc38eb71bc4e7472ceeed50f4675b9dc5ddf1464
gkHell <gpkmyworld@gmail.com>
2023-02-01 05:00:19 +0530

    01/02/2023⠀⎦⎡⠀05:00⠀⎦⎢⠀Wednesday

---
+++ PyProgrammer/CH06_P01_understaning_array.py
@@ -0,0 +1,25 @@
+
+
+arr = [12,'31','Hello','p',{1,2,3,4},[1,2,4,63,67],("Hello",)]
+
+for i in arr:
+    print(i,type(i))
+
+
+import array as ar
+
+a = ar.array('i',[])
+
+# print(a)
+
+# for i in a:
+#     print(i)
+
+x = int(input("Enter the size : "))
+
+for i in range(x):
+    data = int(input("Enter the element : "))
+    a.append(data)
+
+
+print(a)
