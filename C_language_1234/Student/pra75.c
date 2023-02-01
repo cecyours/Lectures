93b4802a7b61e8ec3d1446b42f329868485d1cb2
gkHell <gpkmyworld@gmail.com>
2023-02-01 22:24:23 +0530

    01/02/2023⠀⎦⎡⠀22:24⠀⎦⎢⠀Wednesday

---
+++ C_language_1234/Student/pra75.c
@@ -0,0 +1,27 @@
+
+#include<stdio.h>
+void main()
+{
+    int a[100];
+    int n,i,*p;
+
+    p = &a[0];
+
+    printf("enter the size : ");
+    scanf("%d",&n);
+
+    printf("enter the data : ");
+
+    for(i=0;i<n;i++)
+    {
+        printf("enter data for a[ %d ] = ",i);
+        scanf("%d",&a[i]);
+    }
+
+    printf("------------------------\n");
+
+    for(i=0;i<n;i++)
+    {
+        printf("a[ %d ] = %d\n",i,*(p+i));
+    }
+}
