9d96559a6e49afb3c45d0a14968aa1f6b84a91f3
gkHell <gpkmyworld@gmail.com>
2023-02-03 19:15:17 +0530

    03/02/2023⠀⎦⎡⠀19:15⠀⎦⎢⠀Friday

---
+++ C_language_9099/CH03_P09_logical_or.c
@@ -0,0 +1,18 @@
+
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    char ch;
+
+    printf("do you want to exit : ");
+    scanf("%c",&ch);
+
+    if(ch=='y' || ch=='Y')
+    {
+        printf("exit...");
+        exit(0);
+    }
+    
+    printf("welcome..");
+}
---
+++ C_language_9099/CH03_P10_logical_or_pt2.c
@@ -0,0 +1,18 @@
+
+#include<stdio.h>
+int main()
+{
+    int number;
+    printf("enter a number : ");
+    scanf("%d",&number);
+
+    if(number%3==0 || number%5==0)
+    {
+        printf("welcome");
+    }
+    else
+    {
+        printf("by by..");
+    }
+
+}
