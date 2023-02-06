5c1c35fd48f4710a3db7baf8014bdc3244a4dea7
gkHell <gpkmyworld@gmail.com>
2023-02-06 21:55:29 +0530

    06/02/2023⠀⎦⎡⠀21:55⠀⎦⎢⠀Monday

---
+++ JavaProgramming_2903d/Student/S01.java
@@ -0,0 +1,11 @@
+class S01 {
+
+    public static void main(String[] args) {
+        System.out.println("Aditya");
+        System.out.println("Age 22"); 
+        System.out.println("Address..Dehra('Himachal pardesh')");
+        System.out.println("Class...java programing");
+
+    
+   }
+}
