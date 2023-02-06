cc3f960ac3c47c1d07a407f86185790c6ee47179
gkHell <gpkmyworld@gmail.com>
2023-02-06 20:58:57 +0530

    06/02/2023⠀⎦⎡⠀20:58⠀⎦⎢⠀Monday

---
+++ JavaProgramming_2903d/CH01_P01_helloWorld.java
@@ -0,0 +1,11 @@
+
+
+class CH01_P01_helloWorld{
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hello Aditya.");
+        System.out.println("Hii there...");
+        
+    }
+}
