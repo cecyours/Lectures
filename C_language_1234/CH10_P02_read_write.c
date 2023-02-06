170b38a35535ded010aa14830aed55159cf1bdee
gkHell <gpkmyworld@gmail.com>
2023-02-06 12:08:14 +0530

    06/02/2023⠀⎦⎡⠀12:08⠀⎦⎢⠀Monday

---
+++ C_language_1234/CH10_P02_read_write.c
@@ -0,0 +1,15 @@
+
+#include<stdio.h>
+int main()
+{
+     
+     FILE *file;
+     char fileName[100],data[100];
+
+
+        printf("Enter file name : ");
+        scanf("%s",fileName);
+
+        
+    return 0;
+}
