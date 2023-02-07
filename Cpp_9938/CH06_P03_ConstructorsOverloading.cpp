9af83779d6f4c6c0e07ce0ca2b7b12b5defd8fa4
gkHell <gpkmyworld@gmail.com>
2023-02-07 18:49:30 +0530

    07/02/2023⠀⎦⎡⠀18:49⠀⎦⎢⠀Tuesday

--- Cpp_9938/CH06_P03_ConstructorsOverloading.cpp
+++ Cpp_9938/CH06_P03_ConstructorsOverloading.cpp
@@ -3,15 +3,43 @@
 using namespace std;
 class Student{
 
+    int rollNo;
+    string name;
+
     public:
+    // @default_constructor
      Student()
         {
-            cout<<"hello World "<<endl;
 
-             
+            rollNo = 0;
+            name = "NULL";
         }
+    
+    // @paremeterize_contructor
+    Student(int rollNo,string name)
+    {
+        this->rollNo = rollNo;
+        this->name = name;
+    }
+
+    // @copy_constructor
+
+    Student(Student &s)
+    {
+        this->name = s.name;
+        this->rollNo = s.rollNo;
+    }
+    void display()
+    {
+        cout<<"welcome "<<name<<" , rollNo : "<<rollNo<<endl;
+    }
 };
 int main()
 {
-    Student s1,s2;
+    Student s1,s2(12,"yash"),s3(s2);
+
+    s1.display();
+    s2.display();   
+    s3.display();
+
 }
