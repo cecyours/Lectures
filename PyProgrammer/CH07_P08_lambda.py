be19acf8a44a4833dc536d64a8a368585d117673
gkHell <gpkmyworld@gmail.com>
2023-02-09 05:25:14 +0530

    09/02/2023⠀⎦⎡⠀05:25⠀⎦⎢⠀Thursday

---
+++ PyProgrammer/CH07_P08_lambda.py
@@ -0,0 +1,16 @@
+
+
+x = int(input("Enter the number : "))
+
+square = lambda x:x*x
+print(square(x))
+
+sum = lambda x,y:x+y
+
+
+
+
+
+y = int(input("Enter the number : "))
+total = sum(x,y)
+print(total)
