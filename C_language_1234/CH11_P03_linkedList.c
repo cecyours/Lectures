00e71e57d521938b79609b72ec4b4bc930f09cfb
gkHell <gpkmyworld@gmail.com>
2023-02-10 15:43:50 +0530

    10/02/2023⠀⎦⎡⠀15:43⠀⎦⎢⠀Friday

--- C_language_1234/CH11_P03_linkedList.c
+++ C_language_1234/CH11_P03_linkedList.c
@@ -1,6 +1,7 @@
 
 #include<stdio.h>
 #include<stdlib.h>
+#include<stdbool.h>
 struct Node{
 
     int data;
@@ -60,6 +61,68 @@ void display()
 
 }
 
+
+void search(int num)
+{
+    struct Node *temp;
+    bool f = true;
+    if(head==NULL)
+    {
+        printf("No data is available..\n");
+        return;
+    }
+
+    temp = head;
+
+   
+    while(temp)
+    {
+        if(temp->data==num)
+        {
+            f = false;
+            printf("%d found...\n",num);
+        }
+
+        temp = temp->next;
+    }
+
+    if(f)
+    {
+        printf("%d is not found...\n",num);
+    }
+}
+
+void delete_(int num)
+{
+    struct Node *temp;
+
+    if(head==NULL)
+    {
+        printf("No data to delete...\n");
+        return;
+    }
+
+    temp = head;
+
+         if(head->data==num)
+        {
+            head = head->next;
+            return;
+        }
+    while(temp)
+    {
+
+        if(temp->next->data==num)
+        {
+            temp->next = temp->next->next; 
+            break;
+        }
+
+        temp = temp->next;
+    }
+
+    printf("deleted done..\n");
+}
 int main()
 {
 
@@ -67,6 +130,9 @@ int main()
     insert(20);
     insert(30);
     insert(40);
+
+    search(50);
+    delete_(10);
 
     display();
 }
