1fb1cca105379fea5885b563b3b0f8bdf7176285
im <im@sh1n1g4m1>
2023-02-16 17:53:38 +0530

    16/02/2023⠀⎦⎡⠀17:53⠀⎦⎢⠀Thursday

---
+++ BCA_963/Html/CH4_p1.js
@@ -0,0 +1,10 @@
+// Single line comment
+/*
+    multiline comment 
+*/
+
+// alert("Hello")
+// document.write("VAIBHAV");
+
+// var a=10,b=20,c="Vaibhav";
+// document.write(a);
---
+++ BCA_963/Html/CH4_p2_scope_of_variable.js
@@ -0,0 +1,27 @@
+// function sum()
+// {
+//     var a=10.4,b=30.5;
+//     c=a+b;
+//     document.write(c);
+// }
+// sum()
+
+
+// var a=5.4,b=30.5;
+// function vaibhav()
+// {
+//     c=a+b;
+//     document.write(c);
+// }
+// vaibhav()
+
+
+// var a=5,b=2;
+// function vaibhav()
+// {
+//     c=a*2;
+//     d=b**2;
+//     // document.write(c);
+//     document.write(d);
+// }
+// vaibhav()
