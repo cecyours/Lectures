b8a97a35a3d7e7e178f0b5825331bafb617729c3
gkhell <gpkmyworld@gmail.com>
2023-02-16 08:19:28 +0530

    16/02/2023⠀⎦⎡⠀08:19⠀⎦⎢⠀Thursday

---
+++ TC/BIN/T32.C
@@ -0,0 +1,19 @@
+#include<conio.h>
+#include<stdio.h>
+// my name  is suman.
+/*
+	my name
+	is
+	suman
+	// diufbds jsij
+	 //sdjkvndks
+
+
+  */
+void main()
+{
+
+clrscr();
+	printf("Hello Worlds..");
+getch();
+}
