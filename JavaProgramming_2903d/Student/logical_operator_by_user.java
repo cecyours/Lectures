25af4a8c9f5fb6913a0d22e5f12cddfea4eb5ab9
im <im@sh1n1g4m1>
2023-02-21 12:20:50 +0530

    21/02/2023⠀⎦⎡⠀12:20⠀⎦⎢⠀Tuesday

---
+++ JavaProgramming_2903d/Student/logical_operator_by_user.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class logical_operator_by_user
+{
+    public static void main(String[] nikaljao)
+    {
+        Scanner user = new Scanner(System.in);
+        System.out.print("enter the number:= ");
+        int a = user.nextInt();
+        System.out.print("enter the number:=");
+        int b = user.nextInt();
+        if (a==b || a>=b)
+        {
+            System.out.println("hm y bilkul shi h");
+        }
+        else{
+            System.out.println("are y kay kiya sb glt h kl yaad krke aana smjaa");
+        }
+
+    }
+}
