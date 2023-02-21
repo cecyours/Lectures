de3a5a8e927a8c902a14297e6a9958d8f6a5c533
nova-hash <surajgupta929479@gmail.com>
2023-02-21 09:08:46 +0530

    21/02/2023⠀⎦⎡⠀09:08⠀⎦⎢⠀Tuesday

---
+++ C_language_9099/S03.c
@@ -0,0 +1,20 @@
+
+#include<stdio.h>
+int main()
+{
+
+    int month;
+
+    printf("enter the month : ");
+    scanf("%d",&month);
+
+    if(month==1)
+    {
+        printf("")
+    }
+
+
+
+
+    return 0;
+}
