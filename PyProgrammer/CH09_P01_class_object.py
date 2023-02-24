0e24df56738f8f19f9ec50cf5c0f2edceff66374
gkhell <gpkmyworld@gmail.com>
2023-02-24 18:43:47 +0530

    24/02/2023⠀⎦⎡⠀18:43⠀⎦⎢⠀Friday

---
+++ PyProgrammer/CH09_P01_class_object.py
@@ -0,0 +1,18 @@
+
+
+class Operation:
+    def sum(me,a,b):
+        print(a,"+",b,"=",a+b)
+    
+    
+
+x = int(input("Enter the number : "))
+y = int(input("enter the number "))
+
+p = Operation()
+
+p.sum(x,9)
+
+k= Operation()
+k.sum(x,y)
+
