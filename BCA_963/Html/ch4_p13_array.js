747c2c97edd36d6dcf2ab7be11f5c29ec3e43149
im <im@sh1n1g4m1>
2023-02-24 18:07:18 +0530

    24/02/2023⠀⎦⎡⠀18:07⠀⎦⎢⠀Friday

---
+++ BCA_963/Html/ch4_p13_array.js
@@ -0,0 +1,2 @@
+var arr = [1,"vaibhav","a",12.6]
+document.write(arr[3])
---
+++ BCA_963/Html/ch4_p14_array_using_loop.js
@@ -0,0 +1,11 @@
+var arr = new Array("Lembo","R35","Mustang","Corvett","Impala","RR","jeep")
+for(i=1;i<=arr.length;i++)
+{
+    document.write("car= "+arr[i]+"<br>")
+}
+
+// document.write(arr)
+
+
+// car=lambo
+// car=r35
