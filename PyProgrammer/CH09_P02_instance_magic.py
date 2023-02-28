d1d2ae349f8c3a4f10fa316da7050aa95f646975
gkhell <gpkmyworld@gmail.com>
2023-02-28 18:43:25 +0530

    28/02/2023⠀⎦⎡⠀18:43⠀⎦⎢⠀Tuesday

---
+++ PyProgrammer/CH09_P02_instance_magic.py
@@ -0,0 +1,21 @@
+
+
+class Task:
+
+    def __init__(self,name): #contructor
+        self.name = name
+        print("i am ",self.name)
+    
+
+    def __del__(self): # destructor
+        print("i am deleted ",self.name)
+
+
+obj1 = Task("Rupesh")
+obj2 = Task("Krishna")
+
+
+del obj1 # manual delete
+input("i am waiting..")
+
+
