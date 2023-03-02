9b0bae301c616109aef48b608a6be9e62e98c34d
im <im@sh1n1g4m1>
2023-03-02 16:23:58 +0530

    02/03/2023⠀⎦⎡⠀16:23⠀⎦⎢⠀Thursday

---
+++ BCA_963/Html/ch4_p18_event_handling.js
@@ -0,0 +1,42 @@
+// function bgcoloro()
+// {
+//     alert("hello bg red")
+//     document.bgColor="red"
+// }
+
+// alert("hello");
+// document.write("buy")
+
+
+// function add()
+// {
+//     var a,b;
+//     a = document.getElementById("preeti").value
+//     b = document.getElementById("kurmi").value
+//     c = parseInt(a)+parseInt(b)
+//     document.getElementById("ans").innerHTML="Sum= "+c
+// }
+
+
+// function mul()
+// {
+//     var a,b;
+//     a=document.getElementById("king").value
+//     b= document.getElementById("queen").value
+//     c=parseInt(a)*parseInt(b)
+//     document.getElementById("result").innerHTML="multiplication= "+c
+
+// }
+
+// function mmove()
+// {
+//     document.getElementById("dusra_naam").innerHTML="Preeti"
+//     document.getElementById("dusra_naam").style.color="purple"
+//     document.getElementById("chalunga").innerHTML="Kurmi"
+//     document.getElementById("chalunga").style.color="Red"
+// }
+
+function mover()
+{
+    document.getElementById("para").style.backgroundColor="#ff4500";
+}
