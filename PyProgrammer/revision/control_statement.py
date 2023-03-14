81923edd560d0f2a63a950a085908356eafb549f
Ravan <62199279+Lankapatii@users.noreply.github.com>
2023-03-14 17:43:19 +0530

    Add files via upload

---
+++ PyProgrammer/revision/control_statement.py
@@ -0,0 +1,53 @@
+x = int(input("Enter the number x= "))
+y = int(input("Enter the number y= "))
+
+a = int(input("Enter the number a= "))
+b = int(input("Enter the number b= "))
+
+#---------------------------------------------- If-else--------------------------------------------------
+
+if(x == y):
+    print("hello")
+else:
+    print("lol")
+
+z = x+y
+
+#----------------------------------------------------Nested if-else---------------------------------------
+
+# if(z == c):
+#     mul = z*c
+#     if(mul < z):
+#         print("Nested done")
+#     else:
+#         print("Failed")
+# else:
+#     print("Full fail")
+
+#----------------------------------------------------Leader if-else --------------------------------------
+
+# if(z == 10):
+#     c = a+b
+#     print("this is if ",c)
+
+# elif(z == 20):
+#     c = a+b
+#     print("This is else-if 1= ",c)
+
+# elif(z == 30):
+#     c = a+b
+#     print("This is else-if 2= ",c)
+
+# elif(z == 40):
+#     c = a+b
+#     print("This is else-if 3= ",c)
+
+# elif(z == 50):
+#     c = a+b
+#     print("This is else-if 4= ",c)
+
+# else:
+#     print("Thak Gaya")
+
+
+
