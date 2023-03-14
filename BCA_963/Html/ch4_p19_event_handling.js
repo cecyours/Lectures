3247ed0d985202a7f974fe5fcf1cbf4ef8bc9891
im <im@sh1n1g4m1>
2023-03-14 17:39:14 +0530

    14/03/2023⠀⎦⎡⠀17:39⠀⎦⎢⠀Tuesday

---
+++ BCA_963/Html/ch4_p19_event_handling.js
@@ -0,0 +1,30 @@
+function mout()
+{
+    document.getElementById("list1").style.backgroundColor="#ff4500"
+
+}
+
+function mdown()
+{
+    document.getElementById("preeti").style.backgroundColor="lightgreen"
+}
+function mup()
+{
+    document.getElementById("kurmi").style.backgroundColor="yellowgreen"
+}
+
+function key__press()
+{
+    var text = document.getElementById("i1").value
+    document.getElementById("s1").innerHTML=text
+}
+function key__up()
+{
+    var text = document.getElementById("i2").value
+    document.getElementById("s2").innerHTML=text
+}
+function key__down()
+{
+    var text = document.getElementById("i3").value
+    document.getElementById("s3").innerHTML=text
+}
