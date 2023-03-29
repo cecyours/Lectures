a2f978a94cf105f2e60cecc5ed4b943fe52fa9bd
im <im@sh1n1g4m1>
2023-03-29 18:14:14 +0530

    29/03/2023⠀⎦⎡⠀18:14⠀⎦⎢⠀Wednesday

---
+++ PyProgrammer/revision/loops_in_kk.py
@@ -0,0 +1,14 @@
+
+
+# var1 = {"vaibhav","shakshi","neha","ruchita","hina","rakhi","jena","richa"}
+# var1 = 0
+for i in range(2):
+    print(i)
+
+
+# var3=0
+# var2=int(input("enter the start number= "))
+# var1=int(input("enter the range of where you want to go= "))
+# for i in range(var2,var1):
+#     var3 += i
+# print(var3)
