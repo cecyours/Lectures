248831a9b77880b99a9d77bd1ed4569a8f2483e8
Lankapatii <imvaibhavmishra2207@gmail.com>
2023-04-12 19:28:41 +0530

    12/04/2023⠀⎦⎡⠀19:28⠀⎦⎢⠀Wednesday

---
+++ Cpp_9938/student/revision2/Break.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    for (int i = 1; i < 10; i++)
+    {
+        if (i==6)
+        {
+            cout<<"Break statement is in work";
+            break;
+        }
+        cout<<i<<endl;
+    }
+    
+
+
+}
