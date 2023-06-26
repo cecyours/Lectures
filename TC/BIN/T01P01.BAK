0ea5c0668c4acc6a87233fd0bee32a6a806ebef8
gkhell <gpkmyworld@gmail.com>
2023-06-26 18:00:31 +0530

    26/06/2023⠀⎦⎡⠀18:00⠀⎦⎢⠀Monday

---
+++ TC/BIN/T01P01.BAK
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int i=0;
+clrscr();
+
+	for(i=0;i<=10000;i++)
+	{
+		textattr(i);
+		textbackground(BLACK);
+		cprintf("%c",3);
+
+	}
+
+
+getch();
+}
---
+++ TC/BIN/T01P02.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	clrscr();
+
+	textattr(12);
+	cprintf("Hello Laxman.\n\r");
+
+	textattr(14);
+	cprintf("Hello Gk\n\r");
+
+	textattr(2);
+	cprintf("the book is mine");
+
+	getch();
+}
